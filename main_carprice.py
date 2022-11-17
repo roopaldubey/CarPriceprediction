bc21f4f4bcba35c9aa5a63ecbdde15ea59a34f7c
roopaldubey <47363361+roopaldubey@users.noreply.github.com>
2022-11-17 12:05:23 +0530

    Update main_carprice.py

--- main_carprice.py
+++ main_carprice.py
@@ -12,7 +12,7 @@ app.secret_key = 'secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'roopal'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pythonregister'
 
 mysql = MySQL(app);
